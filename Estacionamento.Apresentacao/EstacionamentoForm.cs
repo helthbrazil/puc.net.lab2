cf35179bcee81ca6087f8bc9de3a06aa0e4fd4cb
helthbrazil <hebertalvesferreira@gmail.com>
2017-07-16 14:43:21 -0300

    inicial

---
+++ Estacionamento.Apresentacao/EstacionamentoForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Estacionamento.Apresentacao
+{
+    public partial class EstacionamentoForm : Form
+    {
+        public EstacionamentoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string placa = textBox1.Text;
+
+            try
+            {
+                var proxy = new EstacionamentoService.Service1Client();
+                var valor = proxy.Run(placa);
+
+                proxy.Close();
+
+                MessageBox.Show(String.Format("Placa '{0}' valor de R${1}.", placa, valor));
+                textBox1.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
